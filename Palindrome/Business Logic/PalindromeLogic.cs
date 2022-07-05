0b74de17bacaafc60f29af3539488a259bd47a1a
Duska <Duska@DESKTOP-O3AR66J>
2022-07-05 14:22:14 +0200

    Add project files.

---
+++ Palindrome/Business Logic/PalindromeLogic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palindrome.Business_Logic
+{
+    public class PalindromeLogic
+    {
+        public static bool isThePhrasePalindrome(string str)
+        {
+            int start = 0;
+            int length = str.Length-1;
+
+            str = str.ToLower();
+
+            while (start < length)
+            {
+                char char1 = str[start];
+                char char2 = str[length];
+                if (!(char1 >= 'a' && char1 <= 'z'))
+                    start++;
+                else if (!(char2 >= 'a' && char2 <= 'z'))
+                    length--;
+                else if (char1 == char2)
+                { 
+                    start++;
+                    length--;
+                }
+                else return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Palindrome/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Palindrome.Business_Logic;
+
+namespace Palindrome
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Madam, I'm Adam.";
+            Console.WriteLine(PalindromeLogic.isThePhrasePalindrome(str));   
+        }
+    }
+}
